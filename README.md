c05b1c102848e043b757f9d7235451ca42f181bf
Dima Snisarenko <snisarenkodima@gmail.com>
2026-07-15 12:45:39 +0200

    chore: init

---
+++ README.md
@@ -0,0 +1,7 @@
+# The Curriculum Vitae of Dima Snisarenko
+
+[[Check this out]](https://sneas.github.io/cv-nederlands)
+
+---
+
+See [The Curriculum Vitae Project](https://github.com/sneas/cv-project) for more info about CV's automation with GitHub.
---
+++ src/metadata/metadata.js
@@ -0,0 +1,113 @@
+module.exports = {
+  name: 'Dimah Sneas',
+  title: 'Software Engineer',
+  baseUrl: 'https://sneas.github.io/cv/',
+  facts: {
+    Residence: '<a href="https://goo.gl/maps/3JZUv4LVbG1bmtwN9"><i class="fa fa-home fact-icon"></i>Weesp, NL</a>',
+    Website: '<a href="https://sneas.io"><i class="fa fa-globe fact-icon"></i>sneas.io</a>',
+    LinkedIn: '<a href="https://www.linkedin.com/in/sneas"><i class="fab fa-linkedin fact-icon"></i>sneas</a>',
+    GitHub: '<a href="https://github.com/sneas"><i class="fab fa-github fact-icon"></i>sneas</a>',
+    Email: '<a href="mailto:d@sneas.io"><i class="fa fa-envelope fact-icon"></i>d@sneas.io</a>',
+  },
+  skills: [
+    ['TypeScript', 100],
+    ['AWS', 75],
+    ['HTML5', 100],
+    ['CSS', 100],
+    ['React', 100],
+    ['Angular', 100],
+    ['RxJS', 100],
+    ['OAuth', 100],
+    ['REST', 100],
+    ['GraphQL', 75],
+    ['Scrum', 100],
+    ['CI/CD', 100],
+    ['TDD', 100],
+    ['Terraform', 100],
+    ['NodeJS', 100],
+    ['SQL', 100]
+  ],
+  positions: [
+    {
+      company: 'Vocably',
+      location: 'Weesp, NL',
+      title: 'Founder, Software Engineer',
+      period: 'Nov 2021 - Present',
+      duration: '5 years',
+      skills: ['AWS', 'GCP', 'Terraform', 'NodeJS', 'React Native', 'Angular', 'OAuth', 'Browser Extensions', 'TypeScript'],
+      contents: `
+As the only founder and software engineer of an [open-source language-learning platform](https://github.com/vocably/getvocably), I am fully responsible for the entire product lifecycle. From ideation to revenue generation including customer support, marketing, and GDPR compliance. The platform is growing slowly, but steadily without much babysitting on my side. The platform includes:
+
+- AWS/GCP cost-optimized infrastructure that is running AI prompts and caching the results 
+- iOS and Android apps created with React Native
+- Web app created with Angular
+- Browser extension created with Web Components
+- Thousands of SEO-friendly landing pages created with Vanilla TypeScript
+
+Users [love](https://docs.google.com/spreadsheets/d/15CfNa_Lp1HO5oZDPk4dofJP9cIJexlrnOXvvZ5dcaNo/edit?gid=0#gid=0) this product, and 25% of its growth is attributed to word of mouth.
+`
+    },
+    {
+      company: 'Air France/KLM',
+      location: 'Amstelveen, NL',
+      title: 'Software Engineer, Contract',
+      period: 'Nov 2022 - Nov 2024',
+      duration: '2 years',
+      skills: ['NodeJS', 'TypeScript', 'CI/CD', 'GitHub Actions', 'Kubernetes', 'Angular', 'A/B Testing'],
+      contents: `
+ A software/DevOps engineer for Air France and KLM. If you've purchased tickets with KLM or Air France since 2022, you've encountered my code. The system I maintained (as part of a team, of course) generates approximately €1 billion each month.
+`,
+      review: {
+        file: 'klm-transversal.png',
+        author: 'Sander Verweij, Product Owner',
+      },
+    },
+    {
+      company: 'DAZN',
+      location: 'Amsterdam, NL',
+      title: 'Platform Engineer',
+      period: 'Jul 2019 – Jan 2021',
+      duration: '1.5 years',
+      skills: ['AWS', 'Terraform', 'NodeJS', 'Next.JS', 'GraphQL', 'React', 'Mobx', 'Redux', 'Tailwind', 'Webpack', 'Jest', 'Storybook'],
+      review: {
+        file: 'dazn.png',
+        author: 'Cirpo Cinelly, Engineering Manager',
+      },
+      contents: `
+DX engineer for [DAZN](https://dazn.com) - a leading sports streaming platform.
+`,
+    },
+    {
+      company: 'KLM Royal Dutch Airlines',
+      location: 'Schiphol, NL',
+      title: 'Senior Frontend Developer, Contract',
+      period: 'Feb 2017 – Jun 2019',
+      duration: '2.5 years',
+      skills: ['Angular', 'Ionic', 'RxJS', 'NgRx', 'TypeScript', 'SASS', 'Webpack', 'Jest'],
+      contents: `
+Bootstrapped, maintained, and passed over to another team an internal mobile app.
+
+My users (KLM mechanics at Schiphol) loved the app, and two of my managers referred me for another contract between 2022 and 2024 (see above).
+`,
+      review: {
+        file: 'klm.png',
+        author: 'Önder Ceylan, Tech Lead',
+      }
+    },
+    {
+      company: 'upwork.com',
+      location: 'Mountain View, USA',
+      title: 'Senior Frontend Developer, Contract',
+      period: 'Oct 2012 – Jun 2014',
+      duration: '1.8 years',
+      skills: ['Javascript', 'jQuery', 'qUnit', 'Grunt', 'Selenium', 'Compass/SASS', 'PHP', 'Zend', 'Postgres'],
+      contents: `
+I helped the biggest freelance marketplace, [Upwork](https://www.upwork.com/), to build and perfect the Job Posting part of their website.
+`,
+      review: {
+        file: 'upwork.png',
+        author: 'Sean Kane, Vice President of Engineering',
+      }
+    }
+  ],
+};
